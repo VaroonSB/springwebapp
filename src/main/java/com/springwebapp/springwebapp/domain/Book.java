846721a769f535a0c49bfa8b229e87b36c0d04d6
VaroonSB <varoonsb1311@gmail.com>
2024-06-03 20:32:20 +0530

    added author and book entity

---
+++ src/main/java/com/springwebapp/springwebapp/domain/Book.java
@@ -0,0 +1,39 @@
+package com.springwebapp.springwebapp.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String title;
+    private String isbn;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
