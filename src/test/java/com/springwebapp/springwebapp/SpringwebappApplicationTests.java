74fc35e345dc13436daacc50d250cfd158abcd18
VaroonSB <varoonsb1311@gmail.com>
2024-06-03 20:05:44 +0530

    initialized spring application

---
+++ src/test/java/com/springwebapp/springwebapp/SpringwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springwebapp.springwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
