aa2852394d00b40e6a733ce6690a84123057ff98
VaroonSB <varoonsb1311@gmail.com>
2024-06-05 19:09:52 +0530

    used qualifiers when multiple implementations are available

--- src/main/java/com/springwebapp/springwebapp/services/GreetingServiceImpl.java
+++ src/main/java/com/springwebapp/springwebapp/services/GreetingServiceImpl.java
@@ -1,9 +1,8 @@
 package com.springwebapp.springwebapp.services;
 
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
-@Service
+@Service("baseGreetingService")
 public class GreetingServiceImpl implements GreetingService {
     @Override
     public String sayGreeting() {
