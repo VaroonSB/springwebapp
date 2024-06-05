e53b22c192dd7c2c2d08d652d6796be23f7207b2
VaroonSB <varoonsb1311@gmail.com>
2024-06-05 18:43:24 +0530

    added Primary bean to indicate autowire when two impl are available

--- src/main/java/com/springwebapp/springwebapp/services/GreetingServicePrimary.java
+++ src/main/java/com/springwebapp/springwebapp/services/GreetingServicePrimary.java
@@ -3,10 +3,11 @@ package com.springwebapp.springwebapp.services;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
+@Primary
 @Service
-public class GreetingServiceImpl implements GreetingService {
-    @Override
+public class GreetingServicePrimary implements GreetingService {
+
     public String sayGreeting() {
-        return "Hi from base service";
+        return "Hi from primary base";
     }
 }
