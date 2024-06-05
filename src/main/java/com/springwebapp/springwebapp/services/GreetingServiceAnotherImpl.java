ae20bd65d5a2d64bf0aa239c1fb4f71732a30d02
VaroonSB <varoonsb1311@gmail.com>
2024-06-05 19:25:18 +0530

    used @profile when two service bean has same name to diff

--- src/main/java/com/springwebapp/springwebapp/services/GreetingServiceAnotherImpl.java
+++ src/main/java/com/springwebapp/springwebapp/services/GreetingServiceAnotherImpl.java
@@ -3,11 +3,11 @@ package com.springwebapp.springwebapp.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("baseImpl")
+@Profile("anotherImpl")
 @Service("baseGreetingService")
-public class GreetingServiceImpl implements GreetingService {
+public class GreetingServiceAnotherImpl implements GreetingService{
     @Override
     public String sayGreeting() {
-        return "Hi from base service";
+        return "Hi from another baseService";
     }
 }
--- src/test/java/com/springwebapp/springwebapp/controllers/MyControllerTest.java
+++ src/test/java/com/springwebapp/springwebapp/controllers/MyControllerTest.java
@@ -3,9 +3,11 @@ package com.springwebapp.springwebapp.controllers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ActiveProfiles("anotherImpl")
 @SpringBootTest
 class MyControllerTest {
 
